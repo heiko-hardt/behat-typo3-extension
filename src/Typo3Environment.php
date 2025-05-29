55f1dc5daf87575799a9afb3aaed8f256a795942
Heiko Hardt <heiko@hardt.dev>
2025-05-29 20:55:37 +0000

    [TASK] Add factory for Typo3Environment

--- src/Typo3Environment.php
+++ src/Typo3Environment.php
@@ -2,17 +2,21 @@
 
 namespace HeikoHardt\Behat\TYPO3Extension;
 
-use HeikoHardt\Behat\TYPO3Extension\Typo3\V06\Environment as Typo3v06Environment;
-use HeikoHardt\Behat\TYPO3Extension\Typo3\V07\Environment as Typo3v07Environment;
-use HeikoHardt\Behat\TYPO3Extension\Typo3\V08\Environment as Typo3v08Environment;
-use HeikoHardt\Behat\TYPO3Extension\Typo3\V09\Environment as Typo3v09Environment;
-use HeikoHardt\Behat\TYPO3Extension\Typo3\V10\Environment as Typo3v10Environment;
-use HeikoHardt\Behat\TYPO3Extension\Typo3\V11\Environment as Typo3v11Environment;
-use HeikoHardt\Behat\TYPO3Extension\Typo3\V12\Environment as Typo3v12Environment;
-use HeikoHardt\Behat\TYPO3Extension\Typo3\V13\Environment as Typo3v13Environment;
+use HeikoHardt\Behat\TYPO3Extension\Factory\Typo3EnvironmentFactory;
 
 class Typo3Environment
 {
+
+    /**
+     * @var Typo3EnvironmentFactory
+     */
+    private $Typo3EnvironmentFactory;
+
+    public function __construct()
+    {
+        $this->Typo3EnvironmentFactory = new Typo3EnvironmentFactory();
+    }
+
     public function boot(array $configuration)
     {
         if ($typo3environment = $this->getTypo3Environment()) {
@@ -23,33 +27,8 @@ class Typo3Environment
 
     private function getTypo3Environment()
     {
-        switch ($this->getVersion()) {
-            case '6.2':
-                return new Typo3v06Environment();
-                break;
-            case '7.6':
-                return new Typo3v07Environment();
-                break;
-            case '8.7':
-                return new Typo3v08Environment();
-                break;
-            case '9.5':
-                return new Typo3v09Environment();
-                break;
-            case '10.4':
-                return new Typo3v10Environment();
-                break;
-            case '11.5':
-                return new Typo3v11Environment();
-                break;
-            case '12.4':
-                return new Typo3v12Environment();
-                break;
-            case '13.4':
-                return new Typo3v13Environment();
-                break;
-        }
-        return false;
+        $version = $this->getVersion();
+        return $this->Typo3EnvironmentFactory->createEnvironment($version);
     }
 
     protected function getVersion()
